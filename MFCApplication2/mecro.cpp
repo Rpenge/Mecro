47659cba0c533ca46c1cc1138204fed38ffe9a7b
Rpenge <yoonsojin00@gmail.com>
2022-01-26 18:14:43 +0900

    basic form

---
+++ MFCApplication2/mecro.cpp
@@ -0,0 +1,56 @@
+﻿// mecro.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "MFCApplication2.h"
+#include "mecro.h"
+#include "afxdialogex.h"
+
+
+// mecro 대화 상자
+
+IMPLEMENT_DYNAMIC(mecro, CDialogEx)
+
+mecro::mecro(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+mecro::~mecro()
+{
+}
+
+void mecro::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(mecro, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &mecro::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &mecro::OnBnClickedButton2)
+END_MESSAGE_MAP()
+
+
+// mecro 메시지 처리기
+
+
+void mecro::OnBnClickedButton1()
+{
+	/*click();	//1~4, 6~9
+	esc();
+	time();		//6번 정도 제자리 반복
+	sides();*/	//A~D 
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
+
+
+void mecro::OnBnClickedButton2()
+{
+	/*click();	//1~4, 6~9
+	esc();
+	time();		//6번 정도 제자리 반복
+	updown()*/	//W~S
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
---
+++ MFCApplication2/mecro.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+
+// mecro 대화 상자
+
+class mecro : public CDialogEx
+{
+	DECLARE_DYNAMIC(mecro)
+
+public:
+	mecro(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~mecro();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+};
